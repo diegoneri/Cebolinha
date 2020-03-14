b78067792ca72936b516e85240317fb5a15acc5e
Informática <Informtica@adolphoberezin.com>
2020-03-13 21:58:14 -0300

    Inclui exercicio resolvido.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//[console: Cebolinha] Solicite que o usuário digite uma frase. Exiba a frase como se ela fosse dita pelo Cebolinha, da Turma da Mônica.
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, digite uma frase:");
+            string frase = Console.ReadLine();
+            string flase = frase.Replace("r", "l")
+                                .Replace("R","L");
+            Console.WriteLine(flase);
+            
+            //Console.WriteLine($"O usuário digitou {frase} e o Cebolinha falou {frase}");
+        }
+    }
+}
